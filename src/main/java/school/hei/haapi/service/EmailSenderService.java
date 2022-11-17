ac28ac7b2d1474cc908e387e631173e595b247a4
Micael Jerry <hei.jerry.2@gmail.com>
2022-11-17 20:43:26 +0300

    feat : email sender method

---
+++ src/main/java/school/hei/haapi/service/EmailSenderService.java
@@ -0,0 +1,26 @@
+package school.hei.haapi.service;
+
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.stereotype.Service;
+
+@Service
+@Slf4j
+@AllArgsConstructor
+public class EmailSenderService {
+    private JavaMailSender mailSender;
+
+    public void  sendEmail(String toEmail,String subject,String body) {
+        SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom("hei.jerry.2@gmail.com");
+        message.setTo(toEmail);
+        message.setSubject(subject);
+        message.setText(body);
+
+        mailSender.send(message);
+
+        log.info("MAIL SENT SUCCESSFULLY ....");
+    }
+}
