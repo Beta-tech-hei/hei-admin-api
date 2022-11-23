2cdc6e7496cbde2e91773e158819cc75841d23b6
Micael Jerry <hei.jerry.2@gmail.com>
2022-11-23 20:54:24 +0300

    chore: add delay penalty service

---
+++ src/main/java/school/hei/haapi/service/DelayPenaltyService.java
@@ -0,0 +1,35 @@
+package school.hei.haapi.service;
+
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+import school.hei.haapi.model.DelayPenalty;
+import school.hei.haapi.model.validator.DelayPenaltyValidator;
+import school.hei.haapi.repository.DelayPenaltyRepository;
+
+import java.util.List;
+
+@Service
+@AllArgsConstructor
+public class DelayPenaltyService {
+    private DelayPenaltyRepository delayPenaltyRepository;
+    private DelayPenaltyValidator delayPenaltyValidator;
+
+    public List<DelayPenalty> getAll() {
+        return delayPenaltyRepository.getAll();
+    }
+
+    public DelayPenalty getById(String id) {
+        return delayPenaltyRepository.getById(id);
+    }
+
+    public DelayPenalty getDelayPenalty() {
+        return delayPenaltyRepository.getAll().get(0);
+    }
+
+    public DelayPenalty save(DelayPenalty delayPenalty) {
+        delayPenaltyValidator.accept(delayPenalty);
+        delayPenalty
+                .setId(this.getDelayPenalty().getId());
+        return delayPenaltyRepository.save(delayPenalty);
+    }
+}
